719fe4b986e8ae2d511772cd6e3db3d5a8d4158c
mazezen <mazezen24@gmail.com>
2025-11-11 20:23:43 +0800

    array

---
+++ array/array_sequence.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import array
+import pprint
+
+a = array.array('i', range(3))
+
+print('Initial :', a)
+
+a.extend(range(3))
+print('Extended :', a)
+
+print('Slice :', a[2:5])
+
+print('Iterator:')
+print(list(enumerate(a)))
