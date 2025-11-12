e3fd1763b77f54d3b1d83440c647621f8b5ab2ae
mazezen <mazezen24@gmail.com>
2025-11-12 12:20:44 +0800

    struct

---
+++ struct/struct_unpack.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import struct
+import binascii
+
+packed_data = binascii.unhexlify(b'0100000061620000cdcc2c40')
+
+s = struct.Struct('I 2s f')
+unpacked_data = s.unpack(packed_data)
+print('Unpacked Values:', unpacked_data)
