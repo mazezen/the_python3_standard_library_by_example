0b4aa3bc46b530c68166298b0a283be9befc3f39
mazezen <mazezen24@gmail.com>
2025-11-14 01:45:31 +0800

    statistics

---
+++ statistics/statistics_median.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Author mazezen
+
+from statistics import *
+
+data = [1, 2, 2, 5, 10, 12]
+
+print('median : {:0.2f}'.format(median(data)))
+print('low : {:0.2f}'.format(median_low(data)))
+print('high : {:0.2f}'.format(median_high(data)))
---
+++ statistics/statistics_mode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Author mazezen
+
+from statistics import *
+
+data = [1, 2, 2, 5, 10, 12]
+
+print(mode(data))
