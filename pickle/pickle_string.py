b357828722b3d490f9874e5ac2d727940372e257
mazezen <mazezen24@gmail.com>
2025-11-18 16:08:58 +0800

    pickle

---
+++ pickle/pickle_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Author mazezen
+
+import pickle
+import pprint
+
+data = [{'a': 'A', 'b': 2, 'c': 3.0}]
+print('DATA:', end=' ')
+pprint.pprint(data)
+
+data_string = pickle.dumps(data)
+print('PICKLE: {!r}'.format(data_string))
---
+++ pickle/pickle_unpickle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Author mazezen
+
+import pickle
+import pprint
+
+data1 = [{'a': 'A', 'b': 2, 'c': 3.0}]
+print('BEFORE: ', end=' ')
+pprint.pprint(data1)
+
+data1_string = pickle.dumps(data1)
+
+data2 = pickle.loads(data1_string)
+print('AFTER: ', end=' ')
+pprint.pprint(data2)
+
+print('SAME?  :', (data1 is data2))
+print('EQUAL? :', (data1 == data2))
