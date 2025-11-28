403d0b11c4285f53d8fe9042580768f8b399058b
mazezen <mazezen24@gmail.com>
2025-11-28 12:09:01 +0800

    futures

---
+++ futures/futures_process_pool_map.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Author mazezen
+
+from concurrent import futures
+import os
+
+def task(n):
+    return (n, os.getpid())
+
+ex = futures.ProcessPoolExecutor(max_workers=2)
+results = ex.map(task, range(5, 0, -1))
+
+for n, pid in results:
+    print('ran task {} in process {}'.format(n, pid))
